827646796bed2d22cc9c177718b9f455fd869a67
mehtavinit51 <mehtavinit51@gmail.com>
2023-05-19 09:39:31 +0530

    19/05

--- src/page/LinkPage.js
+++ src/page/LinkPage.js
@@ -3,8 +3,19 @@ const LinkPage =()=>{
     return(
         <div>
             <nav>
+                <ul>
+                <li>
+
+                
+
                 <Link to="/">Home</Link>
+                </li>
+                <li>
+
                 <Link to="/AboutUs">AboutUs</Link>
+                </li>
+                
+                </ul>
             </nav>
         </div>
     )
