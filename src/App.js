cd3403ae91c2c316452ce6a829d9929e84165fea
mehtavinit51 <mehtavinit51@gmail.com>
2023-05-20 10:22:19 +0530

    20/05 commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 // import logo from './logo.svg';
-import {Routes, Route , BrowserRouter , Link} from 'react-router-dom';
-import './App.css';
-import HomePage from './page/HomePage';
-import AboutUs from './page/AboutUs';
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import "./App.css";
+import HomePage from "./page/HomePage";
+import AboutUs from "./page/AboutUs";
+import NotFound from "./page/NotFound";
+import { CssStyledPage } from "./page/CssStyledPage";
 // import LinkPage from './page/LinkPage';
 
 function App() {
   return (
     <div>
-    {/* <HomePage/>
+      {/* <HomePage/>
     <AboutUs/> */}
-    <BrowserRouter>
-    <Link to="/">Home</Link>
-    <Link to="/AboutUs">AboutUs</Link>
-    <Routes>
+      <BrowserRouter
+        style={{
+          padding: 10,
+        }}
+      >
+        <Link
+          to="/"
+          style={{
+            padding: 10,
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          to="/AboutUs"
+          style={{
+            padding: 10,
+          }}
+        >
+          AboutUs
+        </Link>
+        <Link
+          to="/CssStyledPage"
+          style={{
+            padding: 10,
+          }}
+        >
+          CssStyledPage
+        </Link>
+        <Link
+          to="/explore"
+          style={{
+            padding: 10,
+          }}
+        >
+          Explore
+        </Link>
+        <Link
+          to="/Contact"
+          style={{
+            padding: 10,
+          }}
+        >
+          Contact
+        </Link>
+        <Routes>
+          {/* <Route path="/" element={<LinkPage/>}> */}
+          <Route index element={<HomePage />} />
+          <Route path="AboutUs" element={<AboutUs />} />
+          <Route path="CssStyledPage" element={<CssStyledPage />} />
+          <Route path="*" element={<NotFound />} />
 
-      {/* <Route path="/" element={<LinkPage/>}> */}
-        <Route index element={<HomePage/>}/>
-        <Route path="AboutUs" element={<AboutUs/>}/>
-
-      {/* </Route> */}
-    </Routes>
-    </BrowserRouter>
-    
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/page/CssStyledPage.js
@@ -0,0 +1,35 @@
+import AppStyle from "../AppStyle.module.css";
+import Logo from '../logo.svg'
+
+// import 
+export const CssStyledPage = () => {
+  return (
+    <div
+      style={{
+        height: "auto",
+        width: "200vw",
+        backgroundColor: "rgb(116, 84, 141)",
+        // display: "flex",
+        // justifyContent: "space-between",
+      }}
+    >
+      <div className={AppStyle.divStyle}>
+        This Container containes the external css
+      </div>
+      <div className={AppStyle.divStyle}>
+        This Container containes the external css
+      </div>
+      <div className={AppStyle.divStyle}>
+        This Container containes the external css
+      </div>
+      <div className={AppStyle.divStyle}>
+        This Container containes the external css
+      </div>
+      {/* <img src={Logo} alt="not found"/> */}
+      <img src={`${process.env.REACT_APP_HOST}logo192.png`} alt="not found"/>
+      <h3>{process.env.REACT_APP_HOST}</h3>
+      <h3>{process.env.REACT_APP_EXAMPLE_VAR}</h3>
+      
+    </div>
+  );
+};
